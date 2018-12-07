c48e97ea24cc803ba72e4d315d4456cdcab7e34b
Lucas Aquila <lucas-aquila@outlook.com>
2018-12-07 11:43:20 -0200

    Acrescentando comentários aos posts

--- src/main/java/com/lucasaquila/workshopmongo/domain/Post.java
+++ src/main/java/com/lucasaquila/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.lucasaquila.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lucasaquila.workshopmongo.dto.AuthorDTO;
+import com.lucasaquila.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,8 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	private AuthorDTO author;
 	
@@ -76,6 +81,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -100,6 +113,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
 
 }
