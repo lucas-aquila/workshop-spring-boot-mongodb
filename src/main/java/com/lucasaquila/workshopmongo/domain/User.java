ce8c2f10c29a671759b5d07d827c628e53f8c28e
Lucas Aquila <lucas-aquila@outlook.com>
2018-12-07 11:23:19 -0200

    Referenciando os posts do usuário

--- src/main/java/com/lucasaquila/workshopmongo/domain/User.java
+++ src/main/java/com/lucasaquila/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.lucasaquila.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //Anota para dizer que é uma coleção do mongodb
@@ -18,6 +21,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	//Indica que está sendo referenciado por outro
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -51,6 +58,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
