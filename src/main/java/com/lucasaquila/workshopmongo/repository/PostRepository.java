e14a06940e6ef2d5d9e18d3bae01eca21295c8a3
Lucas Aquila <lucas-aquila@outlook.com>
2018-12-07 10:54:27 -0200

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lucasaquila/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucasaquila.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasaquila.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
