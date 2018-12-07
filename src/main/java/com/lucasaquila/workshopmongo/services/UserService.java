55a89827751d953f13b65abf934fa2a17129bd9b
Lucas Aquila <lucas-aquila@outlook.com>
2018-12-07 08:58:01 -0200

    Obtendo um usuário por ID

--- src/main/java/com/lucasaquila/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasaquila/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.lucasaquila.workshopmongo.domain.User;
 import com.lucasaquila.workshopmongo.repository.UserRepository;
+import com.lucasaquila.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,14 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = userRepository.findById(id).get();
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 
 }
